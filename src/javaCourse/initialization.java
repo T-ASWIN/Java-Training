d64b9b902d5dd833249e1b9e6e804573c63c9bf7
221IT105 <aswinmani53@gmail.com>
2024-08-12 20:02:35 +0530

    Test Amend

---
+++ src/javaCourse/initialization.java
@@ -0,0 +1,13 @@
+package javaCourse;
+
+public class initialization {
+    public static void main(String[] args) {
+        int x,y,z;
+        //x=y;
+        y=10;
+        x=y;
+        z=50;
+        System.out.println(x+y+z);
+
+    }
+}
